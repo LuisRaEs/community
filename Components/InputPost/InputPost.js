3f222f8af60c21faea3bcba2e3194c69f1b26041
Luis Ramirez <it12@hound-express.com>
2023-09-20 20:33:16 -0600

    cambio en inputpost

--- Components/InputPost/InputPost.js
+++ Components/InputPost/InputPost.js
@@ -1,12 +1,15 @@
 'use client'
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import { useSelector , useDispatch } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import "./InputPost.css"
 
 export default function InputPost() {
     return (
-        <div id="InputPostContainer">
-            <Form.Control as="textarea" aria-label="With textarea" />
-        </div>
+        <Container>
+            <Form.Control as="textarea" aria-label="With textarea" placeholder="NEwPost" style={{height:"150px",borderRadius:"20px"}}/>
+            <Button variant="primary">Send</Button>
+        </Container>
     )
 }
