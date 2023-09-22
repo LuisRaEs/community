f346e11e30bc6f7ae47d163e29befac172c60565
Daniel Heredia <it13@hound-express.com>
2023-09-22 11:10:25 -0800

    correccion de conflicto en sesionjs

--- public/store/slices/sesion/sesion.js
+++ public/store/slices/sesion/sesion.js
@@ -17,15 +17,22 @@ const sesionSlice = createSlice({
     changeLang: (state, action) => {
       return { ...state, lang: action.payload };
     },
+    addError: (state, action) => {
+      if (!state.errors.includes(action.payload))
+        return { ...state, errors: [...state.errors, action.payload] };
+      else return { ...state };
+    },
+    removeError: (state, action) => {
+      let newErrors = state.errors.filter((error) => error != action.payload);
+      return { ...state, errors: newErrors };
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchSesion.pending, (state) => {
       return { ...state, loading: true };
     });
     builder.addCase(fetchSesion.fulfilled, (state, action) => {
-      console.log(action.payload);
       let status = action.payload.status;
-
       if (status !== "ERROR") {
         return { ...state, loading: false, logged: true };
       } else {
@@ -38,5 +45,5 @@ const sesionSlice = createSlice({
   },
 });
 
-export const { changeLang } = sesionSlice.actions;
+export const { changeLang, addError, removeError } = sesionSlice.actions;
 export const sesion = sesionSlice.reducer;
